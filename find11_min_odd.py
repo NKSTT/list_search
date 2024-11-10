37f8a27501463430ee4bbcdb78a4c12533db0714
NKSTT <asultamuratov110@gmail.com>
2024-11-11 02:00:51 +0800

    find misol 11

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,5 +5,8 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
-    return 0
-
+    a = [num for num in data if num % 2 != 0]
+    x = min(a)
+    return x
+print (find_min_odd([1, 8, 2, 8, 5]))
+print (find_min_odd([7, 8, 4, 3, 5]))
