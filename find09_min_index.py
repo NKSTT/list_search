056d3a17eb633f7922b2ba0907cc2b8086e3113a
NKSTT <asultamuratov110@gmail.com>
2024-11-11 01:59:43 +0800

    find misol 9

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,6 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
-
+    a = min(data)
+    return data.index(a)
+print (find_min_index([12, 2, 5, 2, 7, 9, 1]))
