1240f963072b5d1d08f2e4e965903c39728a14ae
NKSTT <asultamuratov110@gmail.com>
2024-11-11 01:57:08 +0800

    find misol 4

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,7 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    a = max(data)
+    x = data.index(a) 
+    return x
+print (find_max_index([1, 2, 3, 4, 5]))
